c4c73626d23d091cea08a7bbe1ca0fa9a7d8b298
Mmmm0987654321 <kingofjinnat.mkofj@gmail.com>
2026-09-07 22:54:51 +0530

    Add Next.js configuration

---
+++ next.config.js
@@ -0,0 +1,1 @@
+/** @type {import('next').NextConfig} */\nconst nextConfig = {\n  reactStrictMode: true,\n  swcMinify: true,\n  webpack: (config, { isServer }) => {\n    config.externals.push('pino-pretty', 'lokijs', 'encoding');\n    return config;\n  },\n};\n\nmodule.exports = nextConfig;\n
